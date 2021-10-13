753f1861d2731543f1d55555bf66caed9d0fd17e
Charl P. Botha <cpbotha@vxlabs.com>
2021-10-13 12:30:29 +0200

    Add support for wayland clipboard

--- save_image_from_clipboard.py
+++ save_image_from_clipboard.py
@@ -15,21 +15,35 @@ minimum. Yay walrus!
 
 """
 
+import argparse
 from pathlib import Path
 import re
 import subprocess
-import sys
 import tempfile
 
 def main():
-    if len(sys.argv) < 2:
-        print("USAGE: save_image_from_clipboard output_filename.png")
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output_filename", help="Output filename, e.g. bleh.png")
+    # https://docs.python.org/3/howto/argparse.html#introducing-optional-arguments
+    # action=store_true sets this as an on/off flag
+    parser.add_argument("--wayland", help="Use Wayland clipboard instead of X clipboard", action="store_true")
+    args = parser.parse_args()
 
-    result = subprocess.run(["xclip", "-selection", "clipboard", "-t", "TARGETS", "-o"], stdout=subprocess.PIPE)
+    if args.wayland:
+        CLIP_CMD = "wl-paste"
+        LIST_CMD = f"{CLIP_CMD} --list"
+        # double { means we want single in the output, so we can interpolate desired_type later
+        SAVE_CMD = f"{CLIP_CMD} -t {{desired_type}}"
+    else:
+        # use xclip by default
+        CLIP_CMD = "xclip -selection clipboard"
+        LIST_CMD = f"{CLIP_CMD} -t TARGETS -o"
+        SAVE_CMD = f"{CLIP_CMD} -t {{desired_type}} -o"
+
+    result = subprocess.run(LIST_CMD.split(), stdout=subprocess.PIPE)
     xc_output = result.stdout.decode("utf-8")
 
-    output_path = Path(sys.argv[1])
+    output_path = Path(args.output_filename)
     # suffix is e.g. ".png" or "" if no extension was supplied
     # remove the dot at the start
     ext = output_path.suffix[1:]
@@ -43,7 +57,7 @@ def main():
     # xclip and be done with it.
     if xc_output.find(desired_type := f"image/{ext}") >= 0:
         with output_path.open("w") as output_file:
-            subprocess.run(["xclip", "-selection", "clipboard", "-t", desired_type, "-o"], stdout=output_file)
+            subprocess.run(SAVE_CMD.format(desired_type=desired_type).split(), stdout=output_file)
 
     # if we don't have the desired output format, dump what we do have, then convert
     elif mo := re.search("image/(.*)$", xc_output, re.MULTILINE):
@@ -52,13 +66,13 @@ def main():
 
         # dump out to temporary file with the correct extension
         with tempfile.NamedTemporaryFile(suffix=f".{ext_avail}", delete=False) as temp_file:
-            subprocess.run(["xclip", "-selection", "clipboard", "-t", f"image/{ext_avail}", "-o"], stdout=temp_file)
+            subprocess.run(SAVE_CMD.format(desired_type=f"image/{ext_avail}").split(), stdout=temp_file)
 
         # then use imagemagick to convert that to the desired output type
         subprocess.run(["convert", temp_file.name, output_path])
 
     else:
-        raise RuntimeError("Clipboard has no image/ types available.")
+        raise RuntimeError("Clipboard has no image/* types available.")
 
 if __name__ == "__main__":
     main()
